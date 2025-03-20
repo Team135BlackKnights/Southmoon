69e63bae2da1fddd879ef46031944e7199c44147
Eaglegrant <grantman522@gmail.com>
2025-03-19 23:05:29 -0400

    AllButCalib

--- setup/start_cameras.sh
+++ setup/start_cameras.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd /Users/kbmarsh88/Programming/135-TSL/Southstar
+cd /Users/pennrobotics/Southmoon
 
 echo "Starting all camera instances..." | tee -a ~/camera_log.txt
 
 # Start 4 instances with different config/calibration files
-/usr/local/bin/python3 init.py --config config_frontCam.json --calibration calibration_frontCam.json &  
-/usr/local/bin/python3 init.py --config config_extraFrontCam.json --calibration calibration_extraFrontCam.json &  
-/usr/local/bin/python3 init.py --config config_backCam.json --calibration calibration_backCam.json &  
-/usr/local/bin/python3 init.py --config config_rightCam.json --calibration calibration_rightCam.json &  
+python3 init.py --config /Users/pennrobotics/Southmoon/setup/config_frontCam.json --calibration /Users/pennrobotics/Southmoon/setup/calibration_frontCam.json &  
+python3 init.py --config /Users/pennrobotics/Southmoon/setup/config_extraFrontCam.json --calibration /Users/pennrobotics/Southmoon/setup/calibration_extraFrontCam.json &  
+python3 init.py --config /Users/pennrobotics/Southmoon/setup/config_backCam.json --calibration calibration_backCam.json &  
+python3 init.py --config /Users/pennrobotics/Southmoon/setup/config_rightCam.json --calibration /Users/pennrobotics/Southmoon/setup/calibration_rightCam.json &  
 
 # Wait to prevent immediate script exit
 wait
