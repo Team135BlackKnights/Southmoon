5efb5fab38fcd422b250dfb0dabead917c7a9df8
Eaglegrant <grantman522@gmail.com>
2025-10-30 18:52:51 -0400

    working calib

--- start_cameras.sh
+++ start_cameras.sh
@@ -31,9 +31,9 @@ run_camera() {
 
 # If you have multiple cameras, add them here:
  run_camera "config.json" "calibration.json" "FrontRightCam" &
- run_camera "config2.json" "calibration.json" "FrontLeftCam" &
- run_camera "config3.json" "calibration.json" "BackRightCam" &
- run_camera "config4.json" "calibration.json" "BackLeftCam" &
+ run_camera "config2.json" "calibration2.json" "FrontLeftCam" &
+# run_camera "config3.json" "calibration3.json" "BackRightCam" &
+# run_camera "config4.json" "calibration4.json" "BackLeftCam" &
 # run_camera "setup/config_rightCam.json" "setup/calibration_rightCam.json" "RightCam" &
 # run_camera "setup/config_extraFrontCam.json" "setup/calibration_extraFrontCam.json" "ExtraFrontCam" &
 
