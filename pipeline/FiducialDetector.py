388ce9c5d95605db5e567b1ce7d18a620fdf2159
Eaglegrant <grantman522@gmail.com>
2025-10-31 10:55:44 -0400

    get from 58 to 60

--- pipeline/FiducialDetector.py
+++ pipeline/FiducialDetector.py
@@ -37,7 +37,7 @@ class ArucoFiducialDetector(FiducialDetector):
         params.useAruco3Detection = True
        # params.minCornerDistanceRate = 0.10
         params.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_APRILTAG
-        params.cornerRefinementMaxIterations = 20  
+        params.cornerRefinementMaxIterations = 18  
         self._aruco_params = params
         
 
